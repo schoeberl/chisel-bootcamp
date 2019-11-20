21d2c3daa6a997f31427085fcdf71eff2e221530
Simon Andersen <simon.thye@gmail.com>
2019-11-20 10:51:22 +0100

    Changed install script to auto-yes

--- scripts/JupyterScalaLinuxInstall.sh
+++ scripts/JupyterScalaLinuxInstall.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 sudo apt update
 sudo apt upgrade
-sudo apt install curl
+sudo apt -y install curl
 
 
 echo "Install python for ubuntu"
 sudo apt-get update
-sudo apt-get install python3.7
+sudo apt-get -y install python3.7
 
 echo Install pip3
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 echo Install Jupyter
-pip3 install jupyter
+pip3 install -y jupyter
 
 echo install openjdk8
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+sudo apt-get -y install openjdk-8-jdk
 
 echo Install sbt and thereby scala
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
 sudo apt-get update
-sudo apt-get install sbt
+sudo apt-get -y install sbt
 
 
 echo Install Almond Scala - kernel for Jupyter
